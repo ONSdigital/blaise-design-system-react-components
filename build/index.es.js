ad5408023a5e50e6641be22d5b34fa138d8cbd2c
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-02-01 18:07:56 +0000

    Initial commit

--- build/index.es.js
+++ build/index.es.js
@@ -10,5 +10,16 @@ function NotProductionWarning() {
                         React.createElement("p", { className: "u-mb-no" }, "This environment is not a production environment. Do not upload any live data to this service.")))))));
 }
 
-export default NotProductionWarning;
+function Footer() {
+    return (React.createElement(React.Fragment, null,
+        React.createElement("footer", { className: "footer" },
+            React.createElement("div", { className: "footer__body", "data-analytics": "footer" },
+                React.createElement("div", { className: "container " },
+                    React.createElement("div", { className: "grid grid--flex grid--between" },
+                        React.createElement("div", { className: "grid__col" },
+                            React.createElement("div", { className: "u-mt-m u-mb-s" },
+                                React.createElement("img", { src: "https://ons-design-system.netlify.app/img/ons-logo-black-en.svg", alt: "Office for National Statistics" })))))))));
+}
+
+export { Footer, NotProductionWarning };
 //# sourceMappingURL=index.es.js.map
