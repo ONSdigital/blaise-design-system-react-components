ccab586715237f391b9d076abcfa2d0a05fae98a
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-02-02 09:56:03 +0000

    Add Type definitions

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+import React = require("react");
+import {ReactElement} from "react";
+
+declare namespace ONSdesign {
+    declare function NotProductionWarning ReactElement;
+    declare function Footer ReactElement;
+}
