2250f1214ebfee0fe018ac310d359420ab5fce5c
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2021-06-30 14:10:20 +0100

    Warning panel for ONS Panel

--- build/index.js
+++ build/index.js
@@ -147,7 +147,9 @@ var ONSPanel = function (props) {
                 React__default['default'].createElement("svg", { className: "svg-icon " + (props.bigIcon === true ? "svg-icon--xl" : ""), viewBox: "0 0 13 10", xmlns: "http://www.w3.org/2000/svg" },
                     React__default['default'].createElement("path", { d: "M14.35,3.9l-.71-.71a.5.5,0,0,0-.71,0h0L5.79,10.34,3.07,7.61a.51.51,0,0,0-.71,0l-.71.71a.51.51,0,0,0,0,.71l3.78,3.78a.5.5,0,0,0,.71,0h0L14.35,4.6A.5.5,0,0,0,14.35,3.9Z", transform: "translate(-1.51 -3.04)" }))),
         props.status === "warn" &&
-            React__default['default'].createElement("span", { className: "panel__icon", "aria-hidden": "true" }, "!"),
+            React__default['default'].createElement(React__default['default'].Fragment, null,
+                React__default['default'].createElement("span", { className: "panel__icon", "aria-hidden": "true" }, "!"),
+                React__default['default'].createElement("span", { className: "u-vh" }, "Warning: ")),
         React__default['default'].createElement("div", { className: "panel__body " + (props.bigIcon === true ? "svg-icon-margin--xl" : "") }, props.children)));
 };
 
