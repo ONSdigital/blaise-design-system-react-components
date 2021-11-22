918bca25418ded110fc37cbf189f430427633f78
Sam Bryant <srbry@hotmail.com>
2021-11-22 09:18:04 +0000

    Refactor GroupedSummary to class, add tests

---
+++ src/components/ONSSummary.test.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Enzyme, { shallow, render } from "enzyme";
+
+import Adapter from "enzyme-adapter-react-16";
+import { SummaryGroupTable, SummaryItemRow, GroupedSummary } from "./ONSSummary";
+
+describe("ONS Summary Group Table test", () => {
+  Enzyme.configure({ adapter: new Adapter() });
+  const groupedSummary = new GroupedSummary([{ title: "test", records: { foo: "bar" } }]);
+
+  it("matches Snapshot", () => {
+    expect(render(<SummaryGroupTable groupedSummary={groupedSummary} />)).toMatchSnapshot();
+  });
+
+  it("should render correctly", () => {
+    expect(shallow(<SummaryGroupTable groupedSummary={groupedSummary} />).exists()).toEqual(true)
+  });
+});
+
+describe("ONS Summary Item Row test", () => {
+  Enzyme.configure({ adapter: new Adapter() });
+
+  it("matches Snapshot", () => {
+    expect(render(<SummaryItemRow fieldName="foo" fieldValue="bar" />)).toMatchSnapshot();
+  });
+
+  it("should render correctly", () => {
+    expect(shallow(<SummaryItemRow fieldName="foo" fieldValue="bar" />).exists()).toEqual(true)
+  });
+});
+
+describe("GroupedSummary as CSV tests", () => {
+  const groupedSummary = new GroupedSummary([
+    { title: "test", records: { foo: "bar" } },
+    { title: "test2", records: { fwibble: "fish" } }
+  ]);
+
+  it("converts a grouped summary to a csv type object", () => {
+    expect(groupedSummary.csv()).toEqual([{
+      foo: "bar",
+      fwibble: "fish"
+    }])
+  })
+})
--- src/components/ONSSummary.tsx
+++ src/components/ONSSummary.tsx
@@ -1,24 +1,29 @@
-import {Data} from "react-csv/components/CommonPropTypes";
-import {FormatTitle} from "../utilities/TextFormatting";
-import React, {ReactElement} from "react";
-
-type GroupedSummary = Group[];
+import { Data } from "react-csv/components/CommonPropTypes";
+import { FormatTitle } from "../utilities/TextFormatting";
+import React, { ReactElement } from "react";
 
 type Group = {
     title: string
     records: Record<string, any>
 }
+class GroupedSummary {
+    groups: Group[];
 
-function GroupedSummaryAsCSV(groupedSummary: GroupedSummary): Data {
-    const records: Data = [];
-    const row: Record<string, any> = {};
-    for (const group of groupedSummary) {
-        for (const record in group.records) {
-            row[record] = group.records[record];
-        }
+    constructor(groups: Group[]) {
+        this.groups = groups;
     }
-    records.push(row);
-    return records;
+
+    csv(): Data {
+        const records: Data = [];
+        const row: Record<string, any> = {};
+        for (const group of this.groups) {
+            for (const record in group.records) {
+                row[record] = group.records[record];
+            }
+        }
+        records.push(row);
+        return records;
+    }
 }
 
 interface SummaryItemProps {
@@ -26,19 +31,19 @@ interface SummaryItemProps {
     fieldValue: any
 }
 
-function SummaryItemRow({fieldName, fieldValue}: SummaryItemProps): ReactElement {
+function SummaryItemRow({ fieldName, fieldValue }: SummaryItemProps): ReactElement {
     return (
         <tbody className="summary__item">
-        <tr className="summary__row summary__row--has-values">
-            <td className="summary__item-title">
-                <div className="summary__item--text">
-                    {FormatTitle(fieldName)}
-                </div>
-            </td>
-            <td className="summary__values" colSpan={2}>
-                {fieldValue}
-            </td>
-        </tr>
+            <tr className="summary__row summary__row--has-values">
+                <td className="summary__item-title">
+                    <div className="summary__item--text">
+                        {FormatTitle(fieldName)}
+                    </div>
+                </td>
+                <td className="summary__values" colSpan={2}>
+                    {fieldValue}
+                </td>
+            </tr>
         </tbody>
     );
 }
@@ -47,16 +52,16 @@ interface SummaryGroupTableProps {
     groupedSummary: GroupedSummary
 }
 
-function SummaryGroupTable({groupedSummary}: SummaryGroupTableProps): ReactElement {
+function SummaryGroupTable({ groupedSummary }: SummaryGroupTableProps): ReactElement {
     const elementList: ReactElement[] = [];
-    for (const group of groupedSummary) {
+    for (const group of groupedSummary.groups) {
         elementList.push(
             <h3 className="summary__group-title">{group.title}</h3>
         );
         const tableFieldsList: ReactElement[] = [];
         for (const field in group.records) {
             tableFieldsList.push(
-                <SummaryItemRow fieldName={field} fieldValue={group.records[field]}/>
+                <SummaryItemRow fieldName={field} fieldValue={group.records[field]} />
             );
         }
         elementList.push(
@@ -72,5 +77,5 @@ function SummaryGroupTable({groupedSummary}: SummaryGroupTableProps): ReactEleme
     );
 }
 
-export type {GroupedSummary, Group, SummaryItemProps, SummaryGroupTableProps};
-export {GroupedSummaryAsCSV, SummaryItemRow, SummaryGroupTable};
+export type { Group, SummaryItemProps, SummaryGroupTableProps };
+export { GroupedSummary, SummaryItemRow, SummaryGroupTable };
