714bae5117433881d143286fcf096a37d9a54c2f
elthorne <46898565+elthorne@users.noreply.github.com>
2022-03-02 15:47:57 +0000

    feat: Accordion Component.
    Component and tests completed.
    
    Reviewed-by: El
    Refs: BLAIS5-3030

--- src/components/Accordion.test.tsx
+++ src/components/Accordion.test.tsx
@@ -1,50 +1,144 @@
 import React from "react";
 import Accordion from "./Accordion";
-import { render, fireEvent } from "@testing-library/react";
+import {render, fireEvent, RenderResult} from "@testing-library/react";
 
 describe("Accordion tests", () => {
     describe("when show all is enabled", () => {
+        let wrapper: RenderResult
+        beforeEach(() => {
+            wrapper = renderAccordionWithShowAllEnabledAndTwoExpandables()
+        });
 
         it("displays a 'Show all' button", async () => {
-            const wrapper = render(<Accordion ShowAllEnabled={true} Expandables={[{ title: "Foo", content: <p>bar</p> }]} />)
-
-            expect(wrapper.getByTestId("accordion-show-all")).toBeDefined()
-            expect(wrapper.getByTestId("accordion-0-button")).toHaveTextContent("Show")
+            showAllButtonIsDefined(wrapper)
         })
 
-        it("expands a single element when 'Show' on that element is clicked", async () => {
-            // the arrow icon changes direction
+        it("expands a single expandable when 'Show' on that expandable is clicked", async () => {
+            expandableIsClosed("Foo", 0, wrapper)
+            expandableIsClosed("Foo Bar", 1, wrapper)
 
+            clickShowOnASingleElement(0, wrapper)
+
+            expandableIsOpen("Foo", 0, wrapper)
+            expandableIsClosed("Foo Bar", 1, wrapper)
         })
 
         describe("and show all is clicked", () => {
             it("expands all accordions", async () => {
-                // the arrow icons change direction
+                clickShowAll(wrapper);
+
+                expandableIsOpen("Foo", 0, wrapper)
+                expandableIsOpen("Foo Bar", 1, wrapper)
             })
 
             it("changes 'Show all' to 'Hide all'", async () => {
-
+                clickShowAll(wrapper)
+                hideAllButtonIsDefined(wrapper)
             })
 
-            it("hides a single element when 'Hide' on that element is clicked", async () => {
-                //    Show all changes back to Show all
-                // the arrow icons change direction
+            it("toggles between 'Hide all' and 'Show all' when the button is clicked", async () => {
+                clickShowAll(wrapper)
+                hideAllButtonIsDefined(wrapper)
+
+                clickHideAll(wrapper)
+                showAllButtonIsDefined(wrapper)
+
+                clickShowAll(wrapper)
+                hideAllButtonIsDefined(wrapper)
+            })
+
+            it("hides a single expandable when 'Hide' on that expandable is clicked", async () => {
+                showAllButtonIsDefined(wrapper)
+                clickShowAll(wrapper)
+                hideAllButtonIsDefined(wrapper)
+                clickHideOnASingleElement(0, wrapper)
+                expandableIsClosed("Foo", 0, wrapper)
+                showAllButtonIsDefined(wrapper)
             })
         })
     })
 
     describe("when show all is not enabled", () => {
-        it("does not display a 'Show all' button", async () => {
+        let wrapper: RenderResult
+        beforeEach(() => {
+            wrapper = renderAccordionWithShowAllDisabledAndTwoExpandables()
+        });
 
+        it("does not display a 'Show all' button", async () => {
+            showAllButtonIsNotDefined(wrapper)
         })
 
-        it("expands a single element when 'Show' on that element is clicked", async () => {
-            const wrapper = render(<Accordion Expandables={[{ title: "Foo", content: <p>bar</p> }]} />)
+        it("initialises with all expandables closed", async () => {
+            expandableIsClosed("Foo", 0, wrapper)
+            expandableIsClosed("Foo Bar", 1, wrapper)
+        })
 
-            expect(wrapper.getByTestId("accordion-0-content")).toHaveAttribute("aria-hidden", "true")
-            fireEvent.click(wrapper.getByTestId("accordion-0-heading"))
-            expect(wrapper.getByTestId("accordion-0-content")).toHaveAttribute("aria-hidden", "false")
-            expect(wrapper.getByTestId("accordion-0-content")).toHaveTextContent("bar")
+        it("expands a single expandable when 'Show' on that expandable is clicked", async () => {
+            expandableIsClosed("Foo", 0, wrapper)
+            clickShowOnASingleElement(0, wrapper)
+            expandableIsOpen("Foo", 0, wrapper)
         })
     })
 })
+
+function renderAccordionWithShowAllEnabledAndTwoExpandables(): RenderResult {
+    return render(
+        <Accordion ShowAllEnabled={true} Expandables={
+            [
+                { title: "Foo", content: <p>bar</p> },
+                { title: "Foo Bar", content: <p>bar foo</p> }
+            ]
+        } />)
+}
+
+function renderAccordionWithShowAllDisabledAndTwoExpandables(): RenderResult {
+    return render(
+        <Accordion Expandables={
+            [
+                { title: "Foo", content: <p>bar</p> },
+                { title: "Foo Bar", content: <p>bar foo</p> }
+            ]
+        } />)
+}
+
+function clickShowAll(wrapper: RenderResult){
+    fireEvent.click(wrapper.getByTestId("accordion-show-all"))
+}
+
+function clickHideAll(wrapper: RenderResult){
+    fireEvent.click(wrapper.getByTestId("accordion-show-all"))
+}
+
+function clickShowOnASingleElement(id: number, wrapper: RenderResult){
+    fireEvent.click(wrapper.getByTestId(`accordion-${id}-button`))
+}
+
+function clickHideOnASingleElement(id: number, wrapper: RenderResult){
+    fireEvent.click(wrapper.getByTestId(`accordion-${id}-button`))
+}
+
+function showAllButtonIsDefined(wrapper: RenderResult) {
+    expect(wrapper.getByTestId("accordion-show-all")).toBeDefined()
+    expect(wrapper.getByTestId("accordion-show-all")).toHaveTextContent("Show all")
+}
+
+function showAllButtonIsNotDefined(wrapper: RenderResult) {
+    expect(wrapper.queryByTestId("accordion-show-all")).toBeFalsy();
+}
+
+function hideAllButtonIsDefined(wrapper: RenderResult) {
+    expect(wrapper.getByTestId("accordion-show-all")).toBeDefined()
+    expect(wrapper.getByTestId("accordion-show-all")).toHaveTextContent("Hide all")
+}
+
+function expandableIsClosed(title: string, id: number, wrapper: RenderResult) {
+    expect(wrapper.getByTestId(`accordion-${id}-heading`)).toHaveTextContent(title)
+    expect(wrapper.getByTestId(`accordion-${id}-button`)).toHaveTextContent("Show")
+    expect(wrapper.getByTestId(`accordion-${id}-content`)).toHaveAttribute("aria-hidden", "true")
+}
+
+function expandableIsOpen(title: string, id: number, wrapper: RenderResult) {
+    expect(wrapper.getByTestId(`accordion-${id}-heading`)).toHaveTextContent(title)
+    expect(wrapper.getByTestId("accordion-0-button")).toHaveTextContent("Hide")
+    expect(wrapper.getByTestId("accordion-0-content")).toHaveAttribute("aria-hidden", "false")
+}
