bf9cb9c358eb7636e2e9b993d417f91e71329f5e
enigma-cloud <57638182+Enigma-cloud@users.noreply.github.com>
2022-11-17 22:26:01 +0000

    test: add tests for header with navigation bar

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,62 +1,196 @@
 import React from "react";
-import { cleanup, fireEvent, render } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { fireEvent, render } from "@testing-library/react";
 import sinon from "sinon";
-import Header from "./Header";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router";
+import Header, { Props } from "./Header";
 
-describe("Header Test", () => {
-    afterEach(() => {
-        cleanup();
-    });
+const testProps: Props = {
+    title: "ONS Service",
+    navigationLinks: [
+        { label: "Home", endpoint: "/" },
+        { label: "Deploy a questionnaire", endpoint: "/deploy" },
+        { label: "View deployment history", endpoint: "/history" },
+        { label: "Check Blaise status", endpoint: "/status" }
+    ],
+};
 
-    const Props = {
-        title: "App Title",
-    };
-
+describe("Check default Header:", () => {
     it("matches Snapshot", () => {
-        const wrapper = render(<Header title={Props.title} />);
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} />
+            </Router>
+        );
         expect(wrapper).toMatchSnapshot();
-    });
-
-    it("matches Snapshot with signout button", () => {
-        const wrapper = render(<Header title={Props.title} signOutButton signOutFunction={jest.fn()} />);
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    it("should not show the signout button by default", () => {
-        const wrapper = render(<Header title={Props.title} />);
-        expect(wrapper.queryAllByText(/Save and sign out/)).toStrictEqual([]);
-        expect(wrapper.queryAllByText(/Sign out/)).toStrictEqual([]);
-    });
-
-    it("shows Sign out button button", () => {
-        const wrapper = render(<Header title={Props.title} signOutButton signOutFunction={jest.fn()} />);
-        expect(wrapper.getByText(/Save and sign out/)).toBeVisible();
-    });
-
-    it("shows Sign out button button with special text", () => {
-        const wrapper = render(<Header title={Props.title} signOutButton noSave signOutFunction={jest.fn()} />);
-        expect(wrapper.getByText(/Sign out/)).toBeDefined();
-        expect(wrapper.queryAllByText(/Save and sign out/)).toStrictEqual([]);
-    });
-
-    it("passes in the Sign out function correctly button", () => {
-        const mockFunction = sinon.spy();
-        const wrapper = render(<Header title={Props.title} signOutButton signOutFunction={mockFunction} />);
-        act(() => {
-            fireEvent.click(wrapper.getByText(/Save and sign out/));
-        });
-
-        expect(mockFunction).toHaveProperty("callCount", 1);
     });
 
     it("should render correctly", () => {
-        const wrapper = render(<Header title={Props.title} />);
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} />
+            </Router>
+        );
         expect(wrapper).toBeDefined();
     });
 
     it("should render with the title displayed", () => {
-        const wrapper = render(<Header title={Props.title} />);
-        expect(wrapper.getByText(Props.title)).toBeVisible();
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} />
+            </Router>
+        );
+        expect(wrapper.getByText(testProps.title)).toBeVisible();
+    });
+
+    it("should not show the signout button by default", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} />
+            </Router>
+        );
+        expect(wrapper.queryAllByText(/Save and sign out/)).toStrictEqual([]);
+        expect(wrapper.queryAllByText(/Sign out/)).toStrictEqual([]);
+    });
+
+    it("should not show the navigation by default", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} />
+            </Router>
+        );
+        expect(wrapper.queryByRole(/navigation/)).toStrictEqual(null);
+    });
+});
+
+describe("Check Header with sign out button:", () => {
+    it("matches Snapshot with sign out button", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} signOutButton signOutFunction={jest.fn()} />)
+            </Router>
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("shows sign out button", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} signOutButton signOutFunction={jest.fn()} />
+            </Router>
+        );
+        expect(wrapper.getByText(/Save and sign out/)).toBeVisible();
+    });
+
+    it("shows sign out button with special text", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} signOutButton noSave signOutFunction={jest.fn()} />
+            </Router>
+        );
+        expect(wrapper.getByText(/Sign out/)).toBeDefined();
+        expect(wrapper.queryAllByText(/Save and sign out/)).toStrictEqual([]);
+    });
+
+    it("passes in the sign out function correctly button", () => {
+        const mockFunction = sinon.spy();
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} signOutButton signOutFunction={mockFunction} />
+            </Router>
+        );
+        fireEvent.click(wrapper.getByText(/Save and sign out/));
+        expect(mockFunction).toHaveProperty("callCount", 1);
+    }); 
+});
+
+describe("Check Header with navigation bar:", () => {
+    it("matches Snapshot with navigation", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} navigationLinks={testProps.navigationLinks} />)
+            </Router>
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("shows the navigation with links", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} navigationLinks={testProps.navigationLinks} />)
+            </Router>
+        );
+        expect(wrapper.queryByRole(/navigation/)).not.toStrictEqual(null);
+        expect(wrapper.getByRole("link", { name: /Home/ })).toBeVisible();
+        expect(wrapper.getByRole("link", { name: /Deploy a questionnaire/ })).toBeVisible();
+        expect(wrapper.getByRole("link", { name: /View deployment history/ })).toBeVisible();
+        expect(wrapper.getByRole("link", { name: /Check Blaise status/ })).toBeVisible();
+    });
+
+    it("navigates to the homepage when clicking the 'Home' link", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} navigationLinks={testProps.navigationLinks} />)
+            </Router>
+        );
+
+        const homeLink =wrapper.getByRole("link", { name: /Home/ });
+
+        fireEvent.click(homeLink);
+        expect(history.location.pathname).toEqual("/");
+    });
+    
+    it("navigates to the homepage when clicking the 'Deploy a questionnaire' link", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} navigationLinks={testProps.navigationLinks} />)
+            </Router>
+        );
+
+        const deployLink =wrapper.getByRole("link", { name: /Deploy a questionnaire/ });
+
+        fireEvent.click(deployLink);
+        expect(history.location.pathname).toEqual("/deploy");
+    });
+
+    it("navigates to the homepage when clicking the 'View deployment history' link", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} navigationLinks={testProps.navigationLinks} />)
+            </Router>
+        );
+
+        const historyLink =wrapper.getByRole("link", { name: /View deployment history/ });
+
+        fireEvent.click(historyLink);
+        expect(history.location.pathname).toEqual("/history");
+    });
+
+    it("navigates to the homepage when clicking the 'Check Blaise status' link", () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router location={history.location} navigator={history}>
+                <Header title={testProps.title} navigationLinks={testProps.navigationLinks} />)
+            </Router>
+        );
+
+        const statusLink =wrapper.getByRole("link", { name: /Check Blaise status/ });
+
+        fireEvent.click(statusLink);
+        expect(history.location.pathname).toEqual("/status");
     });
 });
