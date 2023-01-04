06a2711160c20c503501adab0e86e5075b7d7e56
enigma-cloud <57638182+Enigma-cloud@users.noreply.github.com>
2023-01-04 15:20:49 +0000

    test: try test for upload functionality

--- src/components/ONSUpload.test.tsx
+++ src/components/ONSUpload.test.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { ONSUpload } from "./ONSUpload";
@@ -17,8 +19,8 @@ describe("ONS Upload Test", () => {
         onChange: jest.fn(),
     };
 
-    function wrapper(render: any, props: any) {
-        return render(
+    function wrapper(renderFunc: any, props: any) {
+        return renderFunc(
             <ONSUpload
                 label={props.label}
                 description={props.description}
@@ -31,14 +33,24 @@ describe("ONS Upload Test", () => {
     }
 
     it("matches Snapshot", () => {
-        expect(wrapper(shallow, Props)).toMatchSnapshot();
+        expect(wrapper(render, Props)).toMatchSnapshot();
     });
-
-    it("should render correctly", () => expect(wrapper(shallow, Props).exists()).toEqual(true));
 
     it("should handle a change", () => {
         const input = wrapper(shallow, changeProps);
         input.find("input").simulate("change", { target: { value: "abc" } });
         expect(changeProps.onChange).toHaveBeenCalled();
+    });
+
+    it.skip("should allow the user to select and upload a file", async () => {
+        wrapper(render, changeProps);
+
+        const uploadInput = screen.getByLabelText(/Upload/i);
+        await userEvent.click(uploadInput);
+        await userEvent.upload(uploadInput, testFile);
+
+        expect(uploadInput.files[0]).toStrictEqual(testFile);
+        expect(uploadInput.files.item(0)).toStrictEqual(testFile);
+        expect(uploadInput.files).toHaveLength(1);
     });
 });
