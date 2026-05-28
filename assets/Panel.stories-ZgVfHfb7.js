2416606453b5721fe12a407be1ffbfad3eda722a
Riceyo <richmond@ricey.org>
2026-05-28 09:29:48 +0000

    Deploying to gh-pages from @ ONSdigital/blaise-design-system-react-components@32018f21c6ea388412c232800b305e277506b159 🚀

---
+++ assets/Panel.stories-ZgVfHfb7.js
@@ -0,0 +1,42 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-CaZkqeYb.js";import{n,t as r}from"./Panel-Bxgt-IYb.js";var i,a,o,s,c,l,u,d,f;e((()=>{n(),i=t(),a={title:`Components/Panel`,component:r,argTypes:{children:{control:!1}}},o={args:{id:`panel-info`,children:(0,i.jsx)(`p`,{children:`Some message in an info panel`})}},s={args:{id:`panel-success`,status:`success`,bigIcon:!0,children:(0,i.jsx)(`p`,{children:`Some message in a success panel`})}},c={args:{id:`panel-success-title`,status:`success`,bigIcon:!0,children:(0,i.jsxs)(`div`,{children:[(0,i.jsx)(`p`,{className:`ons-u-fs-r--b ons-u-mb-xs`,children:`Title in a success panel`}),(0,i.jsx)(`p`,{className:`ons-u-mt-no`,children:`And then some subtext`})]})}},l={args:{id:`panel-error`,status:`error`,children:(0,i.jsx)(`p`,{children:`Some message in an error panel`})}},u={args:{id:`panel-warn`,status:`warn`,children:(0,i.jsx)(`p`,{children:`It's a trap!`})}},d={args:{id:`panel-spacious`,status:`info`,children:(0,i.jsx)(`p`,{children:`So much room for activities!`}),spacious:!0}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: "panel-info",
+    children: <p>Some message in an info panel</p>
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: "panel-success",
+    status: "success",
+    bigIcon: true,
+    children: <p>Some message in a success panel</p>
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: "panel-success-title",
+    status: "success",
+    bigIcon: true,
+    children: <div>
+        <p className="ons-u-fs-r--b ons-u-mb-xs">Title in a success panel</p>
+        <p className="ons-u-mt-no">And then some subtext</p>
+      </div>
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: "panel-error",
+    status: "error",
+    children: <p>Some message in an error panel</p>
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: "panel-warn",
+    status: "warn",
+    children: <p>{"It's a trap!"}</p>
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: "panel-spacious",
+    status: "info",
+    children: <p>So much room for activities!</p>,
+    spacious: true
+  }
+}`,...d.parameters?.docs?.source}}},f=[`Info`,`Success`,`SuccessWithTitleAndSubtext`,`Error`,`Warn`,`Spacious`]}))();export{l as Error,o as Info,d as Spacious,s as Success,c as SuccessWithTitleAndSubtext,u as Warn,f as __namedExportsOrder,a as default};
